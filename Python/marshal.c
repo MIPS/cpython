eac11010962bde5384af382d48a6284fdaa9e01c
Irit Katriel <iritkatriel@yahoo.com>
2021-07-23 17:48:32 +0100

    incref/decref the code object

--- Python/marshal.c
+++ Python/marshal.c
@@ -2068,12 +2068,14 @@ _PyCode_Hydrate(PyCodeObject *code)
     Py_XINCREF(rf.refs);
     rf.refs_pos = code->co_hydra_refs_pos;
     rf.ctx = ctx;
+    Py_XINCREF(code);
     ctx->code = code;
 
     PyObject *result = read_object(&rf);
     Py_XDECREF(rf.refs);
     Py_XDECREF(code->co_hydra_context);
     code->co_hydra_context = NULL;
+    Py_XDECREF(ctx->code);
     ctx->code = NULL;
     assert(result == NULL || PyCode_Check(result));
     return (PyCodeObject *)result;
