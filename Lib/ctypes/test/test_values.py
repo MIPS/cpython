61d2ed773aa6e677fbbc3a8042987a42831fd991
Guido van Rossum <guido@python.org>
2021-07-25 07:23:14 +0200

    Fix test_ctypes (marshalled code objects became longer again)

--- Lib/ctypes/test/test_values.py
+++ Lib/ctypes/test/test_values.py
@@ -80,9 +80,9 @@ class PythonValuesTestCase(unittest.TestCase):
                 continue
             items.append((entry.name.decode("ascii"), entry.size))
 
-        expected = [("__hello__", 164),
-                    ("__phello__", -164),
-                    ("__phello__.spam", 164),
+        expected = [("__hello__", 172),
+                    ("__phello__", -172),
+                    ("__phello__.spam", 172),
                     ]
         self.assertEqual(items, expected, "PyImport_FrozenModules example "
             "in Doc/library/ctypes.rst may be out of date")
